12bad5993d6b0c6e9d298d8a543d3128b93333f3
Jhair1122 <jhairurbina15@gmail.com>
2026-06-11 20:14:16 -0500

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -3,7 +3,7 @@
 // Reemplaza estos valores con los de tu proyecto
 // =============================================
 
-const SUPABASE_URL = "https://tmqpawykchvrfjzxghhu.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tmqpawykchvrfjzxghhu.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcXBhd3lrY2h2cmZqenhnaGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NTAwODAsImV4cCI6MjA5NjUyNjA4MH0.uCFWG1VFeZsTY2VV9DZFCavmTlB_Atr177Q5wwpacVM";
 
 // Inicialización del cliente de Supabase
