fe158a4833ea9f96ff01349e08d830f7f6720493
Jhair1122 <jhairurbina15@gmail.com>
2026-06-11 20:07:51 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -6,7 +6,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // ——— CONFIGURACIÓN (mismos valores que supabase-client.js) ———
 const SUPABASE_URL = "https://tmqpawykchvrfjzxghhu.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY_AQUI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcXBhd3lrY2h2cmZqenhnaGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NTAwODAsImV4cCI6MjA5NjUyNjA4MH0.uCFWG1VFeZsTY2VV9DZFCavmTlB_Atr177Q5wwpacVM";
 // ——————————————————————————————————————————————————————————
 
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
